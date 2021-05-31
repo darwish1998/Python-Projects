5eab6dcca9ce9bb276d41ed08601c405c840b2ca
Ahmed <darwish144.ad@gmail.com>
2021-05-31 21:31:26 +0300

    start up for project

---
+++ data.py
@@ -0,0 +1,16 @@
+"""import datetime
+
+x = datetime.datetime.now()
+print(x)
+"""
+file = open("text.txt","r")
+x = file.read()
+print(x)
+
+
+"""
+file = open("darka.txt", "w")
+file.write("Umi\n")
+file.write("Abdullahi\n")
+file.close()
+"""
